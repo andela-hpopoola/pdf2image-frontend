937dd9abea38dce4820e5bb4c6ff5c76ec9e3317
Haruna Popoola <haruna.popoola@andela.com>
2019-07-01 00:08:39 +0100

    Updates

---
+++ precache-manifest.b9f28207743fd3d49309adab32656200.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452dce972f6dbaa56801d452f4b0cf21",
+    "url": "/andela-hpopoola/pdf2image-frontend/index.html"
+  },
+  {
+    "revision": "db199918eb3d5f3bfe33",
+    "url": "/andela-hpopoola/pdf2image-frontend/static/css/main.398e06f7.chunk.css"
+  },
+  {
+    "revision": "0328b65a8118e7a4fac0",
+    "url": "/andela-hpopoola/pdf2image-frontend/static/js/2.dde5bb1b.chunk.js"
+  },
+  {
+    "revision": "db199918eb3d5f3bfe33",
+    "url": "/andela-hpopoola/pdf2image-frontend/static/js/main.66c66aa8.chunk.js"
+  },
+  {
+    "revision": "1b26bae02fbcf00066c1",
+    "url": "/andela-hpopoola/pdf2image-frontend/static/js/runtime~main.45691983.js"
+  }
+]);
