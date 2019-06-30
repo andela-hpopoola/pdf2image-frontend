5cee1de483623ffe93fc7b85151308b3ef7abf64
Haruna Popoola <haruna.popoola@andela.com>
2019-06-30 15:51:14 +0100

    Convert pdf to text

--- src/assets/components/Dashboard.jsx
+++ src/assets/components/Dashboard.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from 'assets/components/Header';
 
 const Dashboard = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const onChangeHandler = event => {
+    console.log(event.target.files[0]);
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const data = new FormData();
+    const url = 'http://localhost:4001/extract-text';
+    data.append('pdf', selectedFile);
+
+    fetch(url, {
+      method: 'POST',
+      body: data
+    })
+      .then(response => {
+        response.json().then(function(data) {
+          console.log(data);
+        });
+      })
+      .catch(error => {
+        console.log('error');
+      });
+  };
+
   return (
     <div className="container">
       <div className="content">
@@ -9,6 +35,20 @@ const Dashboard = () => {
           <Header />
 
           <h3>Welcome to the Dashboard</h3>
+          <div className="container">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group files">
+                <input
+                  type="file"
+                  className="form-control"
+                  onChange={onChangeHandler}
+                />
+              </div>
+              <button type="submit" className="btn btn-danger text-uppercase">
+                Upload
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
