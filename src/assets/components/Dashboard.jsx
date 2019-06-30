e1ca4c53a6d0a689188c2c09768f4e5726469c20
Haruna Popoola <haruna.popoola@andela.com>
2019-06-30 20:27:18 +0100

    Add error messages to upload PDF

--- src/assets/components/Dashboard.jsx
+++ src/assets/components/Dashboard.jsx
@@ -1,15 +1,44 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from 'assets/components/Header';
+import Alert from 'assets/components/Alert';
 
 const Dashboard = () => {
+  const MAX_PDF_SIZE = 1000000; //1MB
   const [selectedFile, setSelectedFile] = useState(null);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [extractedText, setExtractedText] = useState(null);
+  const [conversionTool, setConversionTool] = useState(false);
+
   const onChangeHandler = event => {
-    console.log(event.target.files[0]);
-    setSelectedFile(event.target.files[0]);
+    setConversionTool(false);
+    const file = event.target.files[0];
+    if (file.size > MAX_PDF_SIZE) {
+      setError('Uploaded PDF should be less than 1MB');
+    } else if (file.type !== 'application/pdf') {
+      setError('Uploaded file must be a valid PDF');
+    } else {
+      setSelectedFile(file);
+      setConversionTool(true);
+    }
+  };
+
+  const newLineToBr = text => {
+    return text.split('\n').map((line, key) => (
+      <Fragment key={key}>
+        {line}
+        <br />
+      </Fragment>
+    ));
+  };
+
+  const uploadNewPdf = () => {
+    setSuccess(false);
   };
 
   const handleSubmit = event => {
     event.preventDefault();
+    setSuccess(false);
     const data = new FormData();
     const url = 'http://localhost:4001/extract-text';
     data.append('pdf', selectedFile);
@@ -20,11 +49,15 @@ const Dashboard = () => {
     })
       .then(response => {
         response.json().then(function(data) {
-          console.log(data);
+          const text = newLineToBr(data.result);
+          setExtractedText(text);
+          setConversionTool(false);
+          setSuccess(true);
         });
       })
       .catch(error => {
-        console.log('error');
+        setError(error.message);
+        // actions.setSubmitting(false);
       });
   };
 
@@ -32,23 +65,49 @@ const Dashboard = () => {
     <div className="container">
       <div className="content">
         <div className="dashboard-container">
-          <Header />
+          <Header text="You are logged in as demo@testing.com" />
 
-          <h3>Welcome to the Dashboard</h3>
-          <div className="container">
+          <div className="container pt-3">
             <form onSubmit={handleSubmit}>
-              <div className="form-group files">
-                <input
-                  type="file"
-                  className="form-control"
-                  onChange={onChangeHandler}
-                />
-              </div>
-              <button type="submit" className="btn btn-danger text-uppercase">
-                Upload
-              </button>
+              {error && <Alert message={error} />}
+              {success && (
+                <Fragment>
+                  <Alert
+                    type="success"
+                    message="Your file has been successfully converted"
+                  />
+                  <button className="btn btn-danger" onClick={uploadNewPdf}>
+                    Upload another PDF
+                  </button>
+                </Fragment>
+              )}
+
+              {success ||
+                (conversionTool ? (
+                  <div className="conversion-tool">
+                    <h3>{selectedFile && selectedFile.name}</h3>
+                    <div className="pt-3">
+                      <button
+                        type="submit"
+                        className="btn btn-info text-uppercase"
+                      >
+                        Convert to Text
+                      </button>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="form-group files">
+                    <input
+                      type="file"
+                      className="form-control"
+                      onChange={onChangeHandler}
+                    />
+                  </div>
+                ))}
             </form>
           </div>
+
+          {success && <div className="container">{extractedText}</div>}
         </div>
       </div>
     </div>
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = ({ text }) => {
   return (
     <div className="text-center">
       <h1 className="logo">
         PDF <span>CONVERTER</span>
       </h1>
-      <p className="description">
-        Instantly convert PDF documents to text or image format with this free
-        PDF converter.
-      </p>
+      <p className="description">{text}</p>
     </div>
   );
+};
+
+const appDescription =
+  'Instantly convert PDF documents to text or image format with this free PDF converter.';
+
+Header.propTypes = {
+  text: PropTypes.string
+};
+
+Header.defaultProps = {
+  text: appDescription
 };
 
 export default Header;
